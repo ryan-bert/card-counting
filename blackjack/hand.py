22cc65870ace0a12a0587db38e2aa132ffdce4ae
arch-bert <archbert66@gmail.com>
2024-01-26 00:55:34 +0200

    Added __str__() method

--- blackjack/hand.py
+++ blackjack/hand.py
@@ -6,3 +6,8 @@ class Hand(object):
         self.cards = []
         self.value = 0
         self.aces = 0
+
+    def __str__(self):
+        if not self.cards:
+            return "No cards in hand"
+        return ", ".join(str(card) for card in self.cards)
